95be7d05c118e680abd8c6309a2f38388a38ff41
ayushkr101 <ayushkr101@gmail.com>
2018-11-23 14:30:15 +0530

    Updates

---
+++ precache-manifest.af8d05dd4bb6e51b0277fc0d1a99deab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd4ff1131f182e0a354",
+    "url": "/CP_POC/static/js/runtime~main.1dd4ff11.js"
+  },
+  {
+    "revision": "60ca7948aaa8194afc31",
+    "url": "/CP_POC/static/js/main.60ca7948.chunk.js"
+  },
+  {
+    "revision": "d812b26a713551b934b7",
+    "url": "/CP_POC/static/js/1.d812b26a.chunk.js"
+  },
+  {
+    "revision": "60ca7948aaa8194afc31",
+    "url": "/CP_POC/static/css/main.76428e95.chunk.css"
+  },
+  {
+    "revision": "15798246a620050aaefd14a139d6a1a9",
+    "url": "/CP_POC/index.html"
+  }
+];
